ff4a08a09f9742e53536f972a21ccfe2f0557e98
Sanjshine99 <sanubandara12639@gmail.com>
2023-07-11 20:25:57 +0530

    refactor: updated the round 2 questions

--- script.js
+++ script.js
@@ -1,155 +1,155 @@
-window.addEventListener("DOMContentLoaded", function() {
+window.addEventListener("DOMContentLoaded", function () {
   var r1q1 = document.getElementById("r1q1");
   if (r1q1) {
-    r1q1.addEventListener("click", function() {
+    r1q1.addEventListener("click", function () {
       window.location.href = "Questions/round1/q (1).html";
     });
   }
 
   var r1q2 = document.getElementById("r1q2");
   if (r1q2) {
-    r1q2.addEventListener("click", function() {
+    r1q2.addEventListener("click", function () {
       window.location.href = "Questions/round1/q (2).html";
     });
   }
 
   var r1q3 = document.getElementById("r1q3");
   if (r1q3) {
-    r1q3.addEventListener("click", function() {
+    r1q3.addEventListener("click", function () {
       window.location.href = "Questions/round1/q (3).html";
     });
   }
 
   var r1q4 = document.getElementById("r1q4");
   if (r1q4) {
-    r1q4.addEventListener("click", function() {
+    r1q4.addEventListener("click", function () {
       window.location.href = "Questions/round1/q (4).html";
     });
   }
 
   var r1q5 = document.getElementById("r1q5");
   if (r1q5) {
-    r1q5.addEventListener("click", function() {
+    r1q5.addEventListener("click", function () {
       window.location.href = "Questions/round1/q (5).html";
     });
   }
 
   var r1q6 = document.getElementById("r1q6");
   if (r1q6) {
-    r1q6.addEventListener("click", function() {
+    r1q6.addEventListener("click", function () {
       window.location.href = "Questions/round1/q (6).html";
     });
   }
 
   var r1q7 = document.getElementById("r1q7");
   if (r1q7) {
-    r1q7.addEventListener("click", function() {
+    r1q7.addEventListener("click", function () {
       window.location.href = "Questions/round1/q (7).html";
     });
   }
 
   var r1q8 = document.getElementById("r1q8");
   if (r1q8) {
-    r1q8.addEventListener("click", function() {
+    r1q8.addEventListener("click", function () {
       window.location.href = "Questions/round1/q (8).html";
     });
   }
 
   var r1q9 = document.getElementById("r1q9");
   if (r1q9) {
-    r1q9.addEventListener("click", function() {
+    r1q9.addEventListener("click", function () {
       window.location.href = "Questions/round1/q (9).html";
     });
   }
 
   var r1q10 = document.getElementById("r1q10");
   if (r1q10) {
-    r1q10.addEventListener("click", function() {
+    r1q10.addEventListener("click", function () {
       window.location.href = "Questions/round1/q (10).html";
     });
   }
 
   var r2q1 = document.getElementById("r2q1");
   if (r2q1) {
-    r2q1.addEventListener("click", function() {
-      window.location.href = "Questions/round1/q (1).html";
+    r2q1.addEventListener("click", function () {
+      window.location.href = "Questions/round2/q (1).html";
     });
   }
 
   var r2q2 = document.getElementById("r2q2");
   if (r2q2) {
-    r2q2.addEventListener("click", function() {
-      window.location.href = "Questions/round1/q (2).html";
+    r2q2.addEventListener("click", function () {
+      window.location.href = "Questions/round2/q (2).html";
     });
   }
 
   var r3q1 = document.getElementById("r3q1");
   if (r3q1) {
-    r3q1.addEventListener("click", function() {
+    r3q1.addEventListener("click", function () {
       window.location.href = "Questions/round3/q1.html";
     });
   }
 
   var r3q2 = document.getElementById("r3q2");
   if (r3q2) {
-    r3q2.addEventListener("click", function() {
+    r3q2.addEventListener("click", function () {
       window.location.href = "Questions/round3/q2.html";
     });
   }
 
   var r3q3 = document.getElementById("r3q3");
   if (r3q3) {
-    r3q3.addEventListener("click", function() {
+    r3q3.addEventListener("click", function () {
       window.location.href = "Questions/round3/q3.html";
     });
   }
 
   var r3q4 = document.getElementById("r3q4");
   if (r3q4) {
-    r3q4.addEventListener("click", function() {
+    r3q4.addEventListener("click", function () {
       window.location.href = "Questions/round3/q4.html";
     });
   }
 
   var r3q5 = document.getElementById("r3q5");
   if (r3q5) {
-    r3q5.addEventListener("click", function() {
+    r3q5.addEventListener("click", function () {
       window.location.href = "Questions/round3/q5.html";
     });
   }
 
   var r3q6 = document.getElementById("r3q6");
   if (r3q6) {
-    r3q6.addEventListener("click", function() {
+    r3q6.addEventListener("click", function () {
       window.location.href = "Questions/round3/q6.html";
     });
   }
 
   var r3q7 = document.getElementById("r3q7");
   if (r3q7) {
-    r3q7.addEventListener("click", function() {
+    r3q7.addEventListener("click", function () {
       window.location.href = "Questions/round3/q7.html";
     });
   }
 
   var r3q8 = document.getElementById("r3q8");
   if (r3q8) {
-    r3q8.addEventListener("click", function() {
+    r3q8.addEventListener("click", function () {
       window.location.href = "Questions/round3/q8.html";
     });
   }
 
   var r3q9 = document.getElementById("r3q9");
   if (r3q9) {
-    r3q9.addEventListener("click", function() {
+    r3q9.addEventListener("click", function () {
       window.location.href = "Questions/round3/q9.html";
     });
   }
 
   var r3q10 = document.getElementById("r3q10");
   if (r3q10) {
-    r3q10.addEventListener("click", function() {
+    r3q10.addEventListener("click", function () {
       window.location.href = "Questions/round3/q10.html";
     });
   }
-})
+});
